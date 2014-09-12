5b6948f7b77f72b56482541dbde7b114f0559573
Roberto Haziel Tienda Rodríguez <rohatiro@gmail.com>
2014-09-12 10:16:04 -0500

    Estructuración del proyecto

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-var express, server, bodyparser, swig;
+var express, server, bodyparser, swig, path;
 
 express = require("express");
 bodyparser = require("body-parser");
 swig = require("swig");
+path = require("path");
+
+var rootdir,appdir,serverdir,clientdir,viewdir,assetsdir;
+
+rootdir = path.resolve(__dirname,".");
+appdir = path.resolve(rootdir,"./app");
+serverdir = path.resolve(appdir, "./server");
+clientdir = path.resolve(appdir,"./client");
+viewdir = path.resolve(serverdir,"./views");
+assetsdir = path.resolve(clientdir,"./assets");
 
 server = express();
 
 server.engine("html", swig.renderFile);
 server.set("view engine", "html");
-server.set("views",".");
+server.set("views",viewdir);
 
-server.use(express.static("."));
+server.use(express.static(assetsdir));
 server.use(bodyparser.urlencoded({extended:true}));
 server.use(bodyparser.json());
 
