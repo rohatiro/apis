a47b5ac87f2aa5305d3bc87fecf1e92b3930a954
Roberto Haziel Tienda Rodríguez <rohatiro@gmail.com>
2014-11-04 17:49:23 -0600

    Prueba para obtener el waveform

--- app/client/assets/js/sc.js
+++ app/client/assets/js/sc.js
@@ -4,7 +4,7 @@ var createSound = function(element)
 	var uri = "/soundcloud/tracks/";
 	var options = {};
 	var id = Number($element.attr("id"));
-	var waveform = new Waveform({container:$element.find(".waveform")[0],innerColor:"#333",height:50,copper:true});
+	var waveform = new Waveform({container:$element.find(".waveform")[0],innerColor:"#333",height:50,copper:false});
 	waveform.dataFromSoundCloudTrack({waveform_url:$element.find(".waveform").attr("data-url")});
 
 	var soundoptions = waveform.optionsForSyncedStream();
