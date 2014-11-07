d2128e74e5aa295b9daa8ae0d9cd73a397e669c0
Roberto Haziel Tienda Rodríguez <rohatiro@gmail.com>
2014-11-07 14:50:37 -0600

    Integrando Backbone

--- app/client/assets/js/sc.js
+++ app/client/assets/js/sc.js
@@ -1,23 +1,86 @@
+window._Track = Backbone.Model.extend({
+	initialize:function()
+	{
+		var options = {};
+		var attrs = this.toJSON();
+		var waveform = new Waveform({container:$element.find(".waveform")[0],innerColor:"#555",height:60,croppe:true});
+		waveform.dataFromSoundCloudTrack({waveform_url:$element.find(".waveform").attr("data-url")});
+		var soundoptions = waveform.optionsForSyncedStream();
+
+		options.id = attrs.id;
+		options.url = attrs.track_url;
+		options.whileplaying = soundoptions.whileplaying;
+		options.whileloading = soundoptions.whileloading;
+
+		var sound = soundManager.createSound(options);
+
+		this.waveform = waveform;
+		this.sound = sound;
+	},
+	validate:function(attributes)
+	{
+		if(!attributes.id)
+			return "El modelo necesita la propiedad de 'oid', este  es el id del track de soundcloud";
+		else if(!attributes.element)
+			return "El modelo necesita la propiedad de 'id'";
+		else if(!attributes.waveform_url)
+			return "El modelo necesita la propiedad de 'waveform_url', este es la url del waveform del track de soundlcoud";
+		else if(!attributes.track_url)
+			return "El modelo necesita la propiedad de 'track_url', este es la url del stream del track de soundlcoud";
+		return false;
+	}
+});
+window.Track_Waveform = Backbone.View.extend({
+	events: {
+		"click .track-controls .btn.play": "play",
+		"click .track-controls .btn.pause": "pause"
+	},
+	initialize: function(model)
+	{
+		this.model = model;
+	},
+	play: function(event) {
+		var e = $(event.srcElement || event.currentElement);
+		var parentcontainer = e.parent().parent().parent();
+		var id = Number(parentcontainer.attr("id"));
+		e.removeClass("play").addClass("pause");
+		soundManager.getSoundById(id).play();
+	},
+	pause: function(event) {
+		var e = $(event.srcElement || event.currentElement);
+		var parentcontainer = e.parent().parent().parent();
+		var id = Number(parentcontainer.attr("id"));
+		e.removeClass("pause").addClass("play");
+		soundManager.getSoundById(id).pause();
+	},
+});
+window._Tracks = Backbone.Collection.extend({
+	model:Track
+});
+var createWaveform = function(track)
+{
+	var attrs,id;
+	attrs = track.toJSON();
+	id = attrs.oid;
+	track.el = "#"+id;
+	track.view = new Track_Waveform(track);
+};
 var createSound = function(element)
 {
+	var attrs = {};
 	var $element = $(element);
-	var uri = "/soundcloud/tracks/";
-	var options = {};
 	var id = Number($element.attr("id"));
-	var waveform = new Waveform({container:$element.find(".waveform")[0],innerColor:"#555",height:60,croppe:true});
-	waveform.dataFromSoundCloudTrack({waveform_url:$element.find(".waveform").attr("data-url")});
+	var uri = window.location.href + "/soundcloud/tracks/" + id;
+	attrs.oid = id;
+	attrs.id = "T-"+uri+"-"+Math.random();
+	attrs.waveform_url = $element.find(".waveform").attr("data-url");
+	attrs.track_url = uri;
 
-	var soundoptions = waveform.optionsForSyncedStream();
-
-	options.id = id;
-	options.url = uri+id;
-	options.whileplaying = soundoptions.whileplaying;
-	options.whileloading = soundoptions.whileloading;
-
-	var sound = soundManager.createSound(options);
-	sound.Waveform = waveform;
+	var sound = Tracks.add(attrs);
 	return sound;
 };
+window.Tracks = new _Tracks();
+window.Tracks.on("add",createWaveform);
 $(function() {
 	soundManager.onready(function() {
 		var $tracks;
@@ -26,24 +89,6 @@ $(function() {
 		sounds = [];
 		$tracks = $(".track");
 
-		for(i = 0; i < $tracks.length; i++)
-		{
-			sounds.push(createSound($tracks[i]));
-		}
-
-		window.sounds = sounds;
-	});
-	$(".track-controls").on("click",".btn",function() {
-		var e = $(event.srcElement || event.currentElement);
-		var parentcontainer = e.parent().parent().parent();
-		var id = Number(parentcontainer.attr("id"));
-		if(e.hasClass("play")) {
-			e.removeClass("play").addClass("pause");
-			soundManager.getSoundById(id).play();
-		}
-		else if(e.hasClass("pause")) {
-			e.removeClass("pause").addClass("play");
-			soundManager.getSoundById(id).pause();
-		}
+		for(i = 0; i < $tracks.length; i++) createSound($tracks[i]);
 	});
 });
